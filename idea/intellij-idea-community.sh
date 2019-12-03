2ab47836df90ef8e3a4e09eeab40c05b11a0b4ed
Vitor Lobo Ramos <lobocode@gmail.com>
2019-12-02 22:30:46 -0300

    fix idea install

--- idea/intellij-idea-community.sh
+++ idea/intellij-idea-community.sh
@@ -7,16 +7,16 @@ echo -e "\n Install dependencies\n"
 
 # Fedora
 if [ $lsb_dist == "fedora" ]; then
-	sudo dnf -y "install java-1.8.0-openjdk java-1.8.0-openjdk-devel compat-libstdc++-296.i686 compat-libstdc++-33.i686 compat-libstdc++-33.x86_64 glibc.i686 ncurses-libs.i686"
+	sudo dnf -y install "java-1.8.0-openjdk java-1.8.0-openjdk-devel compat-libstdc++-296.i686 compat-libstdc++-33.i686 compat-libstdc++-33.x86_64 glibc.i686 ncurses-libs.i686"
 fi
 
 if [ $lsb_dist == "ubuntu" ]; then
-	sudo apt -y "install java-1.8.0-openjdk java-1.8.0-openjdk-devel compat-libstdc++-296.i686 compat-libstdc++-33.i686 compat-libstdc++-33.x86_64 glibc.i686 ncurses-libs.i686"
+	sudo apt -y install "java-1.8.0-openjdk java-1.8.0-openjdk-devel compat-libstdc++-296.i686 compat-libstdc++-33.i686 compat-libstdc++-33.x86_64 glibc.i686 ncurses-libs.i686"
 fi
 
 
 CACHEDIR="/tmp/intellij-idea-community";
-mkdir -p "$CACHEDIR"
+sudo mkdir -p "$CACHEDIR"
 cd "$CACHEDIR"
 
 URL=$(wget "https://data.services.jetbrains.com/products/releases?code=IIC&latest=true" -O -| grep -o "https://download.jetbrains.com/idea/ideaIC-[0-9.]*.tar.gz" | head -n 1)
@@ -28,15 +28,15 @@ if [[ ! -f "$FILE" ]]; then
 	exit 1
 fi
 
-tar -xzf "$FILE" -C "/opt/"
+sudo tar -xzf "$FILE" -C "/opt/"
 
-mv /opt/idea-IC* "/opt/intellij-idea-community"
-ln -sf "/opt/intellij-idea-community/bin/idea.sh" "/usr/bin/ideaIC"
+sudo mv /opt/idea-IC* "/opt/intellij-idea-community"
+sudo ln -sf "/opt/intellij-idea-community/bin/idea.sh" "/usr/bin/ideaIC"
 
 xdg-icon-resource install --novendor --size 128 "/opt/intellij-idea-community/bin/idea.png" "intellij-idea-ce"
 gtk-update-icon-cache -f -t /usr/share/icons/hicolor
 
-cat <<EOF | tee /usr/share/applications/intellij-idea-community.desktop
+sudo cat <<EOF | tee /usr/share/applications/intellij-idea-community.desktop
 [Desktop Entry]
 Name=IntelliJ IDEA Community Edition
 Type=Application
