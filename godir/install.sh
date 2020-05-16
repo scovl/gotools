0d565732a9960a637d7c11fab630cb0a04670eca
Vitor Lobo Ramos <lobocode@gmail.com>
2020-05-15 21:19:36 -0300

    easy install godir

--- godir/install.sh
+++ godir/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
 set -e
 
-wget -O /usr/local/bin/godir https://github.com/lobocode/gotools/blob/master/godir/bin/godir
+wget -O /usr/local/bin/godir https://github.com/lobocode/gotools/raw/master/godir/bin/godir
 chmod 755 /usr/local/bin/godir
